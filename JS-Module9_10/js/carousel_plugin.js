e724fb68219c18a65cae8e754f115ad1bbe49e90
Volodymyr Khrystenko <ovod88@bigmir.net>
2016-05-09 12:18:19 +0200

    JS Module 9-10 carousel made by plugin

---
+++ JS-Module9_10/js/carousel_plugin.js
@@ -0,0 +1,36 @@
+$(function() {
+	$('.jcarousel').jcarousel()
+					.jcarousel({
+                		wrap: 'circular'
+            		})
+            		.jcarouselAutoscroll({
+    					interval: 5000
+					});
+
+	$('.jcarousel-prev').click(function(e) {
+    	$('.jcarousel').jcarousel('scroll', '-=1');
+    	e.preventDefault();
+	});
+
+	$('.jcarousel-next').click(function(e) {
+    	$('.jcarousel').jcarousel('scroll', '+=1');
+    	e.preventDefault();
+	});
+
+	$('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .on('click', function(e) {
+                e.preventDefault();
+            })
+            .jcarouselPagination({
+                perPage: 1,
+                item: function(page) {
+                    return '<a href="#' + page + '">' + page + '</a>';
+                }
+    		});
+});
