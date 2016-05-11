90f575bb38e83fb2d88c04fd237aac912b17b9ec
Volodymyr Khrystenko <ovod88@bigmir.net>
2016-05-12 00:30:17 +0200

    JS Module 9-10 carousel made by jQuery without previous button

--- JS-Module9_10/js/carousel_jquery.js
+++ JS-Module9_10/js/carousel_jquery.js
@@ -1,39 +1,123 @@
 $(function() {
 	var carousel = $('.jcarousel');
+	var picture_index = 0;
+	var buttons = $('.jcarousel-pagination a');
 	var img_width = carousel.find('img').first().width();
-	var scrolling_timer = 1000;
+	var default_speed_scrolling  = 1000;
+	var speed_when_clicked = 500;
 	var scroll_timer = 4000;
-	var scroll_interval = setInterval(scrollnext, scroll_timer);
+	var scroll_interval = setInterval(function() {  scrollnext(default_speed_scrolling); }, 
+													scroll_timer);
+
+	buttons.on('click', function(e) {
+		clearInterval(scroll_interval);
+		var current_lis = $('.jcarousel li');
+		var new_picture_index = buttons.index(this);
+		var difference = Math.abs(new_picture_index - picture_index);
+		var $this = $(this);
+
+        if ($this.data('activated')) return false; 
+
+        $this.data('activated', true);
+        setTimeout(function() {
+            $this.data('activated', false)
+        }, default_speed_scrolling); 
+
+        if( new_picture_index > picture_index) {
+		    carousel.find('ul')
+					.animate({left: -difference * img_width}, 
+							speed_when_clicked, 
+							function() {
+					    		current_lis.slice(0, difference).insertAfter(current_lis.last());
+					    		$(this).css({left: 0});
+							});
+        }
+		picture_index = new_picture_index;
+		activateButton();
+
+		setTimeout(scroll_interval = setInterval(function() {  scrollnext(default_speed_scrolling); }, 
+													scroll_timer),default_speed_scrolling);
+        e.preventDefault();
+    });
+
 
 	$('.jcarousel-prev').click(function(e) {
 		clearInterval(scroll_interval);
-		scrollprev();
-		setTimeout(scroll_interval = setInterval(scrollnext, scroll_timer), scroll_timer);
+
+		var $this = $(this);
+        if ($this.data('activated')) return false; 
+
+        $this.data('activated', true);
+        setTimeout(function() {
+            $this.data('activated', false)
+        }, default_speed_scrolling); 
+
+		scrollprev(speed_when_clicked);
+		setTimeout(scroll_interval = setInterval(function() {  scrollnext(default_speed_scrolling); },
+													 scroll_timer), scroll_timer);
     	e.preventDefault();
 	});
 
 	$('.jcarousel-next').click(function(e) {
     	clearInterval(scroll_interval);
-    	scrollnext();
-    	setTimeout(scroll_interval = setInterval(scrollnext, scroll_timer), scroll_timer);
+    	
+    	var $this = $(this);
+        if ($this.data('activated')) return false; 
+
+        $this.data('activated', true);
+        setTimeout(function() {
+            $this.data('activated', false)
+        }, default_speed_scrolling); 
+
+        scrollnext(speed_when_clicked);
+
+    	setTimeout(scroll_interval = setInterval(function() {  scrollnext(default_speed_scrolling); },
+    												 scroll_timer), scroll_timer);
     	e.preventDefault();
 	});
 
-	function scrollnext(){
-		var lis = $('.jcarousel li');
+	function scrollnext(speed){
+		var current_lis = $('.jcarousel li');
+		picture_index++;
+		if( picture_index > current_lis.length - 1) {
+			picture_index = 0;
+		}
+		
+
+		activateButton();
+    	
     	carousel.find('ul')
     			.animate({left: -img_width}, 
-    					scrolling_timer, 
+    					speed, 
     					function() {
-				    		lis.first().insertAfter(lis.last());
+				    		current_lis.first().insertAfter(current_lis.last());
 				    		$(this).css({left: 0});
 						});
 	}
 
-	function scrollprev(){
-		var lis = $('.jcarousel li');
-		lis.last().insertBefore(lis.first());
+	function scrollprev(speed){
+		var current_lis = $('.jcarousel li');
+
+		picture_index--;
+
+		if(picture_index < 0) {
+			picture_index = current_lis.length - 1;
+		}
+		
+		activateButton();
+
+		current_lis.last().insertBefore(current_lis.first());
 		carousel.find('ul').css({left: -img_width});
 		carousel.find('ul')
-    			.animate({left: 0}, scrolling_timer)};
+    			.animate({left: 0}, speed
+    			)
+    };
+
+    function activateButton() {
+    	buttons.eq(picture_index).addClass('active')
+					  .siblings()
+					  .removeClass('active');
+    }
+
+
 }); 
