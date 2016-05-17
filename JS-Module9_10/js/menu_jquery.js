8a5f98a58b23e8993a8ba29391716eff40d3f1a9
Volodymyr Khrystenko <ovod88@bigmir.net>
2016-05-18 01:38:23 +0200

    JS Module 9-10 menu jQuery colors added

--- JS-Module9_10/js/menu_jquery.js
+++ JS-Module9_10/js/menu_jquery.js
@@ -1,4 +1,11 @@
 $(function(){
+	var $submenu1 = $('.submenu_level_1');
+	var $submenu2 = $('.submenu_level_2');
+	var $submenu3 = $('.submenu_level_3');
+	var ref_color;
+
+	ref_color = $submenu1.css('background-color');
+
 	$('.main_menu>li').each(function() {
 		$(this).has('ul').children('a').append('<span>&#9660;</span>');
 	});
@@ -8,10 +15,27 @@ $(function(){
 	
 	$('.main_menu li').hover(
 		function() {
-			$(this).children('ul').slideDown('fast');
+			ref_color = changeRed(ref_color, true);
+			$(this).children('ul').slideDown('fast').css('background-color', ref_color);
 		},
 		function() {
-			$(this).children('ul').slideUp('fast');
+			ref_color = changeRed(ref_color, false);
+			$(this).children('ul').slideUp('fast').css('background-color', ref_color);
 		}
 	);
+
+	function changeRed(rgb_color, flag) {
+		var matched_red = rgb_color.match(/\(\d+/g)[0];
+		var new_red = +matched_red.split('(')[1];
+
+		if(new_red < 255 && flag) {
+			new_red = '(' + (new_red + 50);	
+		} else if (new_red > 0 && !flag) {
+			new_red = '(' + (new_red - 50);
+		} else {
+			new_red = '(' + new_red;
+		}
+
+		return rgb_color.replace(matched_red, new_red);
+	}
 });
