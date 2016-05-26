64c412fe547e40c62797b0373b05c62b0db9ddf9
Volodymyr Khrystenko <ovod88@bigmir.net>
2016-05-26 14:13:50 +0200

    JS Module 13-14 localStorage rendered withour use strict

---
+++ JS-Module13_14/js/generator.js
@@ -0,0 +1,44 @@
+
+var content = {
+	test_title : 'test',
+	questions : [
+		{
+			question : 'Сколько тебе лет?',
+			answers : ['I am 22 years old','I am 23 years old']
+		}, {
+			question : 'Сколько маме лет?',
+			answers : ['She is 40 years old', 'She is 41 years old', 'She is 42 years old', 'She is 43 years old']
+		}, {
+			question : 'Сколько брату лет?',
+			answers : ['He is 30','He is 31', 'He is 32', 'He is 33','He is 34','He is 35','He is 36']
+		}
+	]
+};
+
+
+(function(data, key){
+	var body = document.getElementsByTagName("body")[0];
+	var div = document.createElement('div');
+	var template_code = document.getElementById("test_template").innerHTML;
+	var content;
+
+	content = JSON.parse(checkStorage(key));
+	div.innerHTML = _.template(template_code) (content);
+
+	while (div.children.length > 0) {
+    	body.appendChild(div.children[0]);
+  	}
+
+  	function checkStorage(key) {
+		if(!localStorage[key]) {
+			try {
+				localStorage.setItem(key, JSON.stringify(data));
+			} catch (err) {
+				alert('Write to localStorage denied : ' + err);
+			}
+		}
+
+		return localStorage[key];
+  	} 
+
+})(content, 'test_content');
