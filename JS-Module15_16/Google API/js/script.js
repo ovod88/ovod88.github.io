1b86422af736f58ecba5c506dede74b53e1594c0
Volodymyr Khrystenko <ovod88@bigmir.net>
2016-06-10 13:17:02 +0200

    JS Module 15-16 Google page d'accueil clavier ajoute via template

--- JS-Module15_16/Google API/js/script.js
+++ JS-Module15_16/Google API/js/script.js
@@ -30,11 +30,27 @@ $(function() {
         	$(this).find("em").hide();
     });
 
-
+	$('.transliteracja a').click(function() {
+		var clavier = new VirtualClavier('uk');
+		clavier.init();		
+	});
 
     function VirtualClavier(language) {
+
+    	var content = {
+    		'name': 'Екранна клавіатура',
+    		'line_first' : ["'", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '='],
+    		'line1_first_alt' : ["₴", '!', '"', '№',';', '%', ':', '?', '*', '(', ')', '_', '+']
+
+    	}
     	if(language == 'uk') {
-    		
+    		content['second_line'] = ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ї'];
+    		content['third_line'] = ['ф', 'і', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'є'];
+    		content['fourth_line'] = ['я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.'];
+    	}
+
+    	this.init = function() {
+    		$('body').append(_.template($('#clavier_template').html()) (content));
     	}
     }
 })
