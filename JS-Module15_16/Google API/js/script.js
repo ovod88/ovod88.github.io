a9df77a4a60e6df2823da485cd9d888cd51f13ac
Volodymyr Khrystenko <ovod88@bigmir.net>
2016-06-12 20:41:44 +0200

    JS Module 15-16 Google page d'accueil v1.0

--- JS-Module15_16/Google API/js/script.js
+++ JS-Module15_16/Google API/js/script.js
@@ -7,6 +7,8 @@ $(function() {
 		$search_box.removeClass('hovered');
 		e.stopPropagation();
 	};
+	var clavier;
+	var watchInputInterval = setInterval(watchTextbox, 300);
 
 	$search_input.click(activateSearchLine).focus(activateSearchLine);
 
@@ -24,6 +26,68 @@ $(function() {
 		$(this).removeClass('hovered');
 	});
 
+	function askBing() {
+		var azureKey = btoa(':8IyMlq81z0M+RY63ZD4TyXP2Il8Fye8c83AgkXxM1NE');
+    	var myUrl = 'https://api.datamarket.azure.com/Bing/Search/v1/Web?Query=%27' + $search_input.val() + '%27' + '&$format=json';
+
+		$.ajax({
+      		method: 'post',
+	     	url: myUrl,
+	      	headers: {
+	        	'Authorization': 'Basic ' + azureKey
+	      	},
+	      	success: function(data) {
+		        var ul = document.createElement("ul");
+			    $.each(data.d.results, function(i, val){
+			            var li = document.createElement("li");
+			            li.innerHTML = '<p class="title">' + '<a href="' + val.Url + '">'+ val.Title +
+			            			'</a>' + '</p>' + '<p class = "url">' + 
+			            				val.Url +'</p>' + '<p class="description">' + val.Description + '</p>';                         
+			            ul.appendChild(li);
+			    });
+			    $('body').append(ul);
+	      	}
+	    });
+
+
+
+
+
+
+
+// 		$.getJSON("http://ajax.googleapis.com/ajax/services/search/web?v=1.0?key=" +
+// 						"ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&" +
+// 										"q=PHP&callback=GoogleCallback&context=?",
+// 			function(data){
+//     			var ul = document.createElement("ul");
+// 			    $.each(data.results, function(i, val){
+// 			            var li = document.createElement("li");
+// 			            li.innerHTML = '<a href="'+val.url+'" title="'+val.url+'" target="_blank">'+val.title+"</a> - "+val.content;                            
+// 			            ul.appendChild(li);
+// 			    });
+// 			    $('body').append(ul);
+// });
+	}
+
+	function watchTextbox() {
+		if($search_input.val()) {
+			var $default_search = $('#default_search');
+			$default_search.removeAttr('id').attr('id', 'search').attr('class', 'clearfix');
+			$default_search.find('.logo img, .logo span').remove();
+			$default_search.find('.buttons button').html('<span></span>');
+			clearInterval(watchInputInterval);
+			$(document).keypress(function(e) {
+    			if(e.which == 13) {
+        			askBing();
+    			}
+			});
+
+			$('.buttons').click(function(e) {
+        		askBing();
+			});
+		}
+	}
+
 	$('.transliteracja').hover(function() {
         	$(this).find("em").show();
     	}, function() {
@@ -32,7 +96,7 @@ $(function() {
 
 	$('.transliteracja a').click(function(event) {
 		event.stopPropagation();
-		var clavier = new VirtualClavier('uk');
+		clavier = new VirtualClavier('uk');
 		clavier.init();		
 	});
 
@@ -56,13 +120,13 @@ $(function() {
     		$('body').append(_.template($('#clavier_template').html()) (content));
     		$('<link rel="stylesheet" href="css/vClavier.css" type="text/css" />').insertAfter($('link'));
 
-    		launchHandlers();
+    		launchHandlers(this);
     	}
 
-    	function launchHandlers() {	
+    	function launchHandlers(instance) {	
     		$('.vClavier-line').click(function(event) {
     			event.stopPropagation();
-    			var $this = $(event.target), character;
+    			var $this = $(event.target).is('li') ? $(event.target) : $(event.target).parent(), character;
     			$this.mousedown(function() {
         				$this.css('border', '1px inset #aaa'); 
         			}).mouseup(function() {
@@ -71,6 +135,51 @@ $(function() {
         		if($this.is('li.letter') && $search_box.hasClass('highlighted')) {
         			character = $.trim($this.html());
 					$search_input.val( $search_input.val() + character );
+        		}
+        		if($this.is('li.capslock')) {
+        			if(!isCapslockOn) {
+        				$this.css('border', '1px inset #aaa'); 
+        				isCapslockOn = true;
+        				$this.parents('.vClavier').find('li.letter').each(function() {
+        					$(this).text($(this).text().toUpperCase());
+        				});
+        			} else {
+        				$this.css('border', '1px solid #aaa');
+        				$this.parents('.vClavier').find('li.letter').each(function() {
+        					$(this).text($(this).text().toLowerCase());
+        				});
+        				isCapslockOn = false;
+        			}
+        		}
+        		if($this.is('li.shift')) {
+        			if(!isShiftOn) {
+        				$this.css('border', '1px inset #aaa'); 
+        				isShiftOn = true;
+        				$this.parents('.vClavier').find('li.letter').each(function() {
+        					$(this).text($(this).text().toUpperCase());
+        				});
+        				$(this).parents('.vClavier')
+        									.find('.vClavier-line')
+        									.eq(1).find('li.letter')
+        									.each(function(index) {
+        										$(this).html(content['line1_first_alt'][index]);
+        									});
+        			} else {
+        				$this.css('border', '1px solid #aaa');
+        				$this.parents('.vClavier').find('li.letter').each(function() {
+        					$(this).text($(this).text().toLowerCase());
+        				});
+        				isShiftOn = false;
+        				$(this).parents('.vClavier')
+        									.find('.vClavier-line')
+        									.eq(1).find('li.letter')
+        									.each(function(index) {
+        										$(this).html(content['line_first'][index]);
+        									});
+        			}
+        		}
+        		if($this.is('.space')) {
+        			$search_input.val( $search_input.val() + ' ' );
         		}
 			});
 
