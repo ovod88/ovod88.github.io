f582ccc94056cbc4dad68970db93a90fc6f89eef
Volodymyr Khrystenko <ovod88@bigmir.net>
2016-06-17 17:54:09 +0200

    Grunt uglifing added

--- JS_module17-18/gruntfile.js
+++ JS_module17-18/gruntfile.js
@@ -13,14 +13,30 @@ module.exports = function(grunt) {
     		},
             css : {
                 src: ['Google API/css/src/*.css'],
-                dest: 'Google API/css/dist/style.css'
+                dest: 'Google API/css/dist/style.min.css'
+            }
+        },
+
+        uglify: {
+            js: {
+                src: 'Google API/js/dist/script.min.js',
+                dest: 'Google API/js/dist/script.min.js'
+            }
+        },
+
+        cssmin: {
+            css:{
+                src: 'Google API/css/dist/style.min.css',
+                dest: 'Google API/css/dist/style.min.css'
             }
         }
 
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', ['concat','uglify', 'cssmin']);
 
 };
