346a9aba03976a3ef623448fb13efaaea28a3fa7
Volodymyr Khrystenko <ovod88@bigmir.net>
2016-06-23 18:09:16 +0200

    Logo added

--- JS-Module19-20/gulpfile.js
+++ JS-Module19-20/gulpfile.js
@@ -1,12 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concatCss = require('gulp-concat-css');
 
 gulp.task('sass', function() {
-    gulp.src('styles/src/core.scss')
+    return gulp.src('styles/src/core.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('styles/dist/'));
 });
 
-gulp.task('watch-sass',function() {
-    gulp.watch('styles/src/*.scss',['sass']);
+gulp.task('concatCss', ['sass'], function () {
+    return gulp.src(['styles/dist/reset.css', 'styles/dist/core.css' ])
+        .pipe(concatCss("styles/dist/style.css"))
+        .pipe(gulp.dest('./'));
 });
+
+gulp.task('watch-sass',function() {
+    return gulp.watch('styles/src/*.scss',['sass', 'concatCss']);
+});
+
+gulp.task('default',['sass', 'concatCss']);
