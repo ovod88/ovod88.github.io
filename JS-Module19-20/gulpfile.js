3689521d2c77118ba4eca5d7d2b2af8b8cbaee4a
Volodymyr Khrystenko <ovod88@bigmir.net>
2016-06-26 22:12:38 +0200

    Added navigation box

--- JS-Module19-20/gulpfile.js
+++ JS-Module19-20/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concatCss = require('gulp-concat-css');
+var sprite = require('gulp.spritesmith');
 
 gulp.task('sass', function() {
     return gulp.src('styles/src/core.scss')
@@ -17,5 +18,35 @@ gulp.task('concatCss', ['sass'], function () {
 gulp.task('watch-sass',function() {
     return gulp.watch('styles/src/*.scss',['sass', 'concatCss']);
 });
+
+gulp.task('sprite', function() {
+    var spriteData =
+        gulp.src('img/sprite-social-src/*.*')
+            .pipe(sprite({
+                imgName: 'img/sprite-social-dist/social-icons.png',
+                cssName: '_sprite-social-icons.scss',
+                cssFormat: 'scss',
+                algorithm: 'binary-tree',
+                padding: 1,
+                cssVarMap: function(sprite) {
+                    var temparray = sprite.name.split('-');
+
+                    if(temparray[1] == 'blue') {
+                        sprite.name = temparray[0] + ':hover';
+                    } else {
+                        sprite.name = temparray[0];
+                    }
+                },
+                cssOpts: {
+                    cssSelector: function (sprite) {
+                        return '.' + sprite.name;
+                    }
+                }
+            }));
+
+    spriteData.img.pipe(gulp.dest('img/sprite-social-dist/'));
+    spriteData.css.pipe(gulp.dest('styles/src/'));
+});
+
 
 gulp.task('default',['sass', 'concatCss']);
