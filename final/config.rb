2e4c6882ac493a365b378d15ebe9a5c4a247cfed
Volodymyr Khrystenko <ovod88@bigmir.net>
2016-12-12 22:08:36 +0100

    Final added register section in header

--- final/config.rb
+++ final/config.rb
@@ -1,4 +1,4 @@
-# require 'breakpoint'
+add_import_path "node_modules/breakpoint-sass/stylesheets"
 
 http_path = '/'
 css_dir = 'css/dist'
@@ -8,4 +8,3 @@ fonts_dir = 'fonts/'
 javascripts_dir = 'js/'
 relative_assets = true
 line_comments = true
-add_import_path "node_modules/breakpoint-sass/stylesheets"
