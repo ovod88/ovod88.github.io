2beac3ee468e058d543c047c22bf4fe1b4a1c76e
Volodymyr Khrystenko <ovod88@bigmir.net>
2016-12-12 17:45:46 +0100

    Added compass font-face mixin and Breakpoint module

--- final/config.rb
+++ final/config.rb
@@ -1,7 +1,11 @@
+# require 'breakpoint'
+
 http_path = '/'
 css_dir = 'css/dist'
 sass_dir = 'css/src'
 images_dir = 'img/src/icons'
+fonts_dir = 'fonts/'
 javascripts_dir = 'js/'
 relative_assets = true
 line_comments = true
+add_import_path "node_modules/breakpoint-sass/stylesheets"
