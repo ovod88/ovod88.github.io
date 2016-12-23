dcafe38258253d44e32e2d5f68ebf0e8832be297
Volodymyr Khrystenko <ovod88@bigmir.net>
2016-12-23 12:39:07 +0100

    Final. Completed How it works section

---
+++ nodejs/complete-node-js-developer-course/password-manager_p29/app.js
@@ -0,0 +1,11 @@
+var argv = require('yargs').argv;
+var command = argv._[0];
+console.log(argv);
+
+if(command === 'hello' && typeof argv.name !== 'undefined' && typeof argv.lastname !== 'undefined') {
+    console.log('Hello ' + argv.name + ' ' + argv.lastname + '!');
+} else if (command === 'hello' && typeof argv.name !== 'undefined'){
+    console.log('Hello ' + argv.name +  '!');
+} else {
+    console.log('Hello world!');
+}
