a72cffce742792335a3345400156fddf8075605a
Volodymyr Khrystenko <ovod88@bigmir.net>
2017-01-02 14:30:21 +0100

    Final. Moved to Gulp4. Refactored gulpfile to Gulp4 + added some new tasks

--- final/gulpfile.js
+++ final/gulpfile.js
@@ -1,17 +1,41 @@
-var gulp = require('gulp');
-var compass = require('gulp-compass');
+'use strict';
+
+const gulp = require('gulp');
+const compass = require('gulp-compass');
+const debug = require('gulp-debug');
+//const sourcemaps = require('gulp-sourcemaps');
+const gulpif = require('gulp-if');
+const del = require('del');
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+
 
 gulp.task('compass', function() {
-    gulp.src('css/src/core.scss')
+    return gulp.src('css/src/core.scss')
+        .pipe(debug({title: 'SCSS'}))
         .pipe(compass({
             config_file: './config.rb',
+            sourcemap: isDevelopment,//NODE_ENV=production gulp compass will not create source maps
             css: 'css/dist',
             sass: 'css/src'
-        }))
+        }));
+        //.pipe(gulp.dest('css/dist'));
+        //.pipe(debug({title: 'COMPASS'}))
 });
 
-gulp.task('watch-sass-files',function() {
-    return gulp.watch(['css/src/**/*.scss', 'css/src/*.scss'],['compass']);
+gulp.task('copy-img-prod', function () {
+   return gulp.src('img/src/**/*.{png,jpeg,jpg}', {since: gulp.lastRun('copy-img-prod')})
+       .pipe(debug({title: 'copying images to production'}))
+       .pipe(gulp.dest('img/dist'));
 });
 
-gulp.task('default',['compass']);
+gulp.task('clean', function() {
+   return del(['css/dist', 'img/dist']);
+});
+
+gulp.task('watch',function() {
+    gulp.watch('css/src/**/*.*', gulp.series('compass'));
+});
+
+gulp.task('build', gulp.series('clean', gulp.parallel('compass', 'copy-img-prod'),'watch'));//gulp.parallel(task1, task2)
+
+gulp.task('default',gulp.series('compass'));
