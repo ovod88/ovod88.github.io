6b1cd892054e8a3c7301f8bb94b90b8d6469a6b8
Volodymyr Khrystenko <ovod88@bigmir.net>
2017-01-03 12:15:58 +0100

    Understanding nodejs changed syntax for module5_6 from ES5 to ES6

--- nodejs/understanding-nodejs-course/module5_6/app.js
+++ nodejs/understanding-nodejs-course/module5_6/app.js
@@ -1,19 +1,34 @@
+'use strict';
+
 var EventEmitter = require('events');
 var util = require('util');
 
-function GreetingGr() {
-    this.greeting = 'Hello world';
+
+class GreetingGr extends EventEmitter {//ES6 syntax
+    constructor() {
+        super();
+        this.greeting = 'Hello world';
+    }
+
+    greet(data) {
+        console.log(`${ this.greeting }`);//ES6 syntax for method in prototype
+        this.emit('greet', data);
+    }
 }
 
-util.inherits(GreetingGr, EventEmitter);
+//function GreetingGr() {//ES5 syntax
+//    EventEmitter.call(this);
+//    this.greeting = 'Hello world';
+//}
+//util.inherits(GreetingGr, EventEmitter);
 
-GreetingGr.prototype.greet = function () {
-    console.log(this.greeting);
-    this.emit('greet');
-};
+//GreetingGr.prototype.greet = function () {//ES5 syntax for method in prototype
+//    console.log(this.greeting);
+//    this.emit('greet');
+//};
 
 var greeter1 = new GreetingGr();
-greeter1.on('greet', function() {
-   console.log('GREET IS CALLED');
+greeter1.on('greet', function(data) {
+   console.log(`GREET IS CALLED ${ data }`);
 });
-greeter1.greet();
+greeter1.greet('MY DEAR');
