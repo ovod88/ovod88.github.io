6156e8316dfc842c3f320a41fb05049cef8b962a
Volodymyr Khrystenko <ovod88@bigmir.net>
2017-01-04 14:49:42 +0100

    Final. Compressed files

--- final/gulpfile.js
+++ final/gulpfile.js
@@ -6,24 +6,40 @@ const debug = require('gulp-debug');
 //const sourcemaps = require('gulp-sourcemaps');
 const gulpif = require('gulp-if');
 const del = require('del');
+const newer = require('gulp-newer');
+const cached = require('gulp-cached');
+const path = require('path');
+const autoprefixer = require('gulp-autoprefixer');
+//const watch = require('gulp-watch');
+const remember = require('gulp-remember');
+const imagemin = require('gulp-image');
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 
-gulp.task('compass', function() {
-    return gulp.src('css/src/core.scss')
-        .pipe(debug({title: 'SCSS'}))
+gulp.task('compass', function() {//TODO Now this task takes 2s to complete
+    return gulp.src('css/src/core.scss',{since: gulp.lastRun('compass')})
+        //.pipe(cached('scss'))
+        //.pipe(debug({title: 'SCSS'}))
         .pipe(compass({
             config_file: './config.rb',
             sourcemap: isDevelopment,//NODE_ENV=production gulp compass will not create source maps
             css: 'css/dist',
             sass: 'css/src'
-        }));
-        //.pipe(gulp.dest('css/dist'));
+        })).
+        pipe(autoprefixer())
+        //.pipe(remember('scss'))
+        .pipe(gulp.dest('css/dist'));
         //.pipe(debug({title: 'COMPASS'}))
 });
 
-gulp.task('copy-img-prod', function () {
-   return gulp.src('img/src/**/*.{png,jpeg,jpg}', {since: gulp.lastRun('copy-img-prod')})
+gulp.task('make-img-prod', function () {
+   return gulp.src('img/src/**/*.{png,jpeg,jpg}', {since: gulp.lastRun('make-img-prod')})
+       .on("data", function(file){
+           console.log(file.relative);
+           // here you can filter all your file depends on name/path/etc.
+       })
+       .pipe(newer('img/dist'))
+       .pipe(imagemin())
        .pipe(debug({title: 'copying images to production'}))
        .pipe(gulp.dest('img/dist'));
 });
@@ -34,8 +50,12 @@ gulp.task('clean', function() {
 
 gulp.task('watch',function() {
     gulp.watch('css/src/**/*.*', gulp.series('compass'));
+        //.on('unlink', function (filepath) {
+            //remember.forget('scss', path.resolve(filepath));
+            //delete cached.caches.scss[path.resolve(filepath)];
+        //});
 });
 
-gulp.task('build', gulp.series('clean', gulp.parallel('compass', 'copy-img-prod'),'watch'));//gulp.parallel(task1, task2)
+gulp.task('build', gulp.series('clean', gulp.parallel('compass', 'make-img-prod')));//gulp.parallel(task1, task2)
 
 gulp.task('default',gulp.series('compass'));
